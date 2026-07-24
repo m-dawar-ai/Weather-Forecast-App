de686b6e6b4e55c74c2c93cacd02ca0d26f7a365
Muneeb Dawar <muneebdawar35@gmail.com>
2026-07-25 03:52:52 +0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import requests
+import streamlit as st
 
-API_KEY = "16cb311b1e4681c33483ebddc6bd4276"
+API_KEY = st.secrets["OPENWEATHER_API_KEY"]
 
 def get_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
